dc0c844e9234bac618de31f2c6ede782f461f4c0
Nikolas Lopes <nikolas.alencar@quintoandar.com.br>
2022-08-11 03:45:42 -0400

    :fire: created global interface

--- src/components/IssuesList/index.tsx
+++ src/components/IssuesList/index.tsx
@@ -1,47 +1,29 @@
 import { useQuery } from '@tanstack/react-query'
 import { Link } from 'react-router-dom'
-
-export interface IssueProps {
-  assignee: string
-  comments: Array<string>
-  completedDate: any
-  createdBy: string
-  createdDate: string
-  dueDate: any
-  id: string
-  labels: Array<string>
-  number: number
-  status: string
-  title: string
-}
+import { IssueProps, IssuesProps } from '../../interfaces/global'
 
 export async function fetchIssuesList() {
   const response = await fetch('/api/issues')
   const data = await response.json()
 
-  console.log(data)
-
-  return data as IssueProps[]
+  return data as IssuesProps
 }
 
 export function IssuesList() {
-  const issuesQuery = useQuery<IssueProps[]>(['issues'], () =>
-    fetchIssuesList()
-  )
+  const issuesQuery = useQuery<IssuesProps>(['issues'], () => fetchIssuesList())
 
+  console.log('data', issuesQuery.data)
   return (
     <div>
       <h2>Issues List</h2>
       {issuesQuery.isLoading ? (
         <p>Loading...</p>
       ) : (
-        <ul>
-          {issuesQuery.data?.map((issue) => (
-            <li>
-              <Link to="/issue/1">Issue {issue.number}</Link>
-            </li>
+        <>
+          {issuesQuery.data?.issues.map((issue) => (
+            <p>{issue.title}</p>
           ))}
-        </ul>
+        </>
       )}
     </div>
   )
---
+++ src/interfaces/global.ts
@@ -0,0 +1,17 @@
+export interface IssueProps {
+  assignee: string
+  comments: Array<string>
+  completedDate: any
+  createdBy: string
+  createdDate: string
+  dueDate: any
+  id: string
+  labels: Array<string>
+  number: number
+  status: string
+  title: string
+}
+
+export type IssuesProps = {
+  issues: IssueProps[]
+}
