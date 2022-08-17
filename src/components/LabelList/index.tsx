f4d911f97ec0282ce31ace8d3c38c48116ddbe37
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-08-17 08:41:36 -0400

    :fire: add labelsQuery on LabelList

--- src/components/LabelList/index.tsx
+++ src/components/LabelList/index.tsx
@@ -1,3 +1,23 @@
+import { useLabelsData } from '../../helpers/useLabelsData'
+
 export function LabelList() {
-  return <h3>Labels</h3>
+  const labelsQuery = useLabelsData()
+
+  console.log(labelsQuery.data)
+  return (
+    <div>
+      <h3>Labels</h3>
+      {labelsQuery.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {labelsQuery.data?.map((label) => (
+            <li key={label.id}>
+              <button className={`${label.color}`}>{label.name}</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
 }
