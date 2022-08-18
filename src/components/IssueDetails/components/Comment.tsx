7e3c637489ecbe9dbe54b489a67013791778b2f9
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-08-18 13:38:03 -0400

    :fire: improve accs on Comment

--- src/components/IssueDetails/components/Comment.tsx
+++ src/components/IssueDetails/components/Comment.tsx
@@ -1,7 +1,27 @@
-export const Comment = ({}) => {
+import { useQuery } from '@tanstack/react-query'
+import { useUserData } from '../../../helpers/useUserData'
+import { IComment } from '../../../interfaces/global'
+
+export const Comment = ({ comment, createdBy, createdDate }: IComment) => {
+  const userQuery = useUserData(createdBy)
+  const user = userQuery.data
+
+  if (userQuery.isLoading) {
+    return (
+      <div className="comment">
+        <div>
+          <div className="comment-header">Loading...</div>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <>
-      <h1>as</h1>
-    </>
+    <div className="comment">
+      <img
+        src={user?.profilePictureUrl}
+        alt={`${user?.name ? user.name : 'Commenter'} Avatar`}
+      />
+    </div>
   )
 }
