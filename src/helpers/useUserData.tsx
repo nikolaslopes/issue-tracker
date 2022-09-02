d2884ece999ec631c6d1f2398d1b3d9fc6392b28
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-09-01 23:22:29 -0400

    :bug: fix useUserData abort signal props

--- src/helpers/useUserData.tsx
+++ src/helpers/useUserData.tsx
@@ -6,21 +6,22 @@ export interface IUser {
   profilePictureUrl: string
 }
 
-export const useUserData = (
-  userId: string | undefined,
-  signal: AbortSignal
-) => {
-  async function fetchUser() {
+export const useUserData = (userId: string | undefined) => {
+  async function fetchUser(signal: AbortSignal | undefined) {
     const response = await fetch(`/api/users/${userId}`, { signal })
     const data: IUser = await response.json()
 
     return data
   }
 
-  const userQuery = useQuery(['user', userId], ({ signal }) => fetchUser, {
-    enabled: userId !== null,
-    staleTime: 1000 * 60 * 5, // 5 minutes
-  })
+  const userQuery = useQuery(
+    ['user', userId],
+    ({ signal }) => fetchUser(signal),
+    {
+      enabled: userId !== null,
+      staleTime: 1000 * 60 * 5, // 5 minutes
+    }
+  )
 
   return userQuery
 }
