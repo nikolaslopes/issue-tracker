d019bf3a60fd223192016a2ea73ce9922e4eb9f3
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-10-13 12:02:17 -0400

    :fire: created createIssue mutation

--- src/pages/AddIssue/index.tsx
+++ src/pages/AddIssue/index.tsx
@@ -1,3 +1,53 @@
-export function AddIssue() {
-  return <h2>Add Issue</h2>
-}
+import { FormEvent } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { createIssue } from './services';
+import { useNavigate } from 'react-router-dom';
+
+export const AddIssue = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const addIssue = useMutation(createIssue, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries(['issues'], { exact: true });
+      queryClient.setQueryData(['issues', String(data.number)], data);
+      navigate(`/issue/${data.number}`);
+    },
+  });
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+    const data = Object.fromEntries(formData);
+
+    if (addIssue.isLoading && !data.title && !data.comment) {
+      return;
+    }
+
+    const payload = {
+      title: String(data.title),
+      comment: String(data.comment),
+    };
+
+    addIssue.mutate(payload);
+  }
+
+  return (
+    <div className="add-issue">
+      <h2>Add Issue</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" name="title" placeholder="Title" />
+
+        <label htmlFor="comment">Comment</label>
+        <textarea id="comment" name="comment" placeholder="Comment" />
+
+        <button type="submit" disabled={false}>
+          {addIssue.isLoading ? 'Adding Issue...' : 'Add Issue'}
+        </button>
+      </form>
+    </div>
+  );
+};
