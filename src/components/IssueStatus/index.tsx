768ea45e8f2071367482ade9c11d7dd98f7b7f4e
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-10-16 23:23:40 -0400

    :fire:

--- src/components/IssueStatus/index.tsx
+++ src/components/IssueStatus/index.tsx
@@ -1,6 +1,9 @@
+import { useMutation } from '@tanstack/react-query';
 import { StatusSelect } from '../StatusSelect';
 
 export const IssueStatus = () => {
+  const statusMutation = useMutation();
+
   return (
     <div className="issue-options">
       <div>
